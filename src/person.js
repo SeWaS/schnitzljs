6316cb11cf998299d958dd6c630b8b20bd84b9dc
SeWa <sebastian_walter@me.com>
2018-10-03 16:10:47 +0200

    A random birthday can be generated for people

--- src/person.js
+++ src/person.js
@@ -19,14 +19,30 @@ export const surname = () => {
     return randomElementInArray(commons);
 };
 
+const ageGroupDef = new Map([
+    ['child', {minAge: 1, maxAge: 9}],
+    ['teenager', {minAge: 10, maxAge: 17}],
+    ['adult', {minAge: 18, maxAge: 59}],
+    ['senior', {minAge: 60, maxAge: 120}]
+]);
+
 export const randomAge = (ageGroup = 'child') => {
-
-    const ageGroupDef = new Map([
-        ['child', {minAge: 1, maxAge: 9}],
-        ['teenager', {minAge: 10, maxAge: 17}],
-        ['adult', {minAge: 18, maxAge: 59}],
-        ['senior', {minAge: 60, maxAge: 120}]
-    ]);
-
     return randomInt(ageGroupDef.get(ageGroup).minAge, ageGroupDef.get(ageGroup).maxAge);
+};
+
+export const randomBirthday = (asString = false, ageGroup = 'child') => {
+    const now = new Date();
+    const randomDate = new Date(
+        now.getFullYear() - randomInt(ageGroupDef.get(ageGroup).minAge, ageGroupDef.get(ageGroup).maxAge),
+        now.getMonth(),
+        now.getDay()
+    );
+
+    if (asString) {
+        const month = randomDate.getMonth() < 10 ? '0' + randomDate.getMonth() : randomDate.getMonth();
+        const day = randomDate.getDay() < 10 ? '0' + randomDate.getDay() : randomDate.getDay();
+        return month + '/' + day + '/' + randomDate.getFullYear();
+    };
+
+    return randomDate;
 };
--- test/person.spec.js
+++ test/person.spec.js
@@ -52,4 +52,102 @@ describe('SchnitzlJS Person...', () => {
 
     })
 
+    describe('can generate a random birthday...', () => {
+
+        const now = new Date()
+
+        it('for a child', () => {
+            const minChild = new Date(
+                now.getFullYear() - 9,
+                now.getMonth(),
+                now.getDay()
+            )
+
+            const maxChild = new Date(
+                now.getFullYear() - 1,
+                now.getMonth(),
+                now.getDay()
+            )
+            _.times(100, () => {
+                expect(Person.randomBirthday()).to.be.at.least(minChild).and.at.most(maxChild)
+            })
+        })
+
+        it('for a child as string', () => {
+            _.times(100, () => {
+                expect(Person.randomBirthday(true)).to.match(/[0-9]{2}\/[0-9]{2}\/[1-2][0-9]{3}/)
+            })
+        })
+
+        it('for a teenager', () => {
+            const minTeen = new Date(
+                now.getFullYear() - 17,
+                now.getMonth(),
+                now.getDay()
+            )
+
+            const maxTeen = new Date(
+                now.getFullYear() - 10,
+                now.getMonth(),
+                now.getDay()
+            )
+            _.times(100, () => {
+                expect(Person.randomBirthday(false, 'teenager')).to.be.at.least(minTeen).and.at.most(maxTeen)
+            })
+        })
+
+        it('for a teenager as string', () => {
+            _.times(100, () => {
+                expect(Person.randomBirthday(true, 'teenager')).to.match(/[0-9]{2}\/[0-9]{2}\/[1-2][0-9]{3}/)
+            })
+        })
+
+        it('for an adult', () => {
+            const minAdult = new Date(
+                now.getFullYear() - 59,
+                now.getMonth(),
+                now.getDay()
+            )
+
+            const maxAdult = new Date(
+                now.getFullYear() - 18,
+                now.getMonth(),
+                now.getDay()
+            )
+            _.times(100, () => {
+                expect(Person.randomBirthday(false, 'adult')).to.be.at.least(minAdult).and.at.most(maxAdult)
+            })
+        })
+
+        it('for an adult as string', () => {
+            _.times(100, () => {
+                expect(Person.randomBirthday(true, 'adult')).to.match(/[0-9]{2}\/[0-9]{2}\/[1-2][0-9]{3}/)
+            })
+        })
+
+        it('for a senior', () => {
+            const minSenior = new Date(
+                now.getFullYear() - 120,
+                now.getMonth(),
+                now.getDay()
+            )
+
+            const maxSenior = new Date(
+                now.getFullYear() - 60,
+                now.getMonth(),
+                now.getDay()
+            )
+            _.times(100, () => {
+                expect(Person.randomBirthday(false, 'senior')).to.be.at.least(minSenior).and.at.most(maxSenior)
+            })
+        })
+
+        it('for a senior as string', () => {
+            _.times(100, () => {
+                expect(Person.randomBirthday(true, 'senior')).to.match(/[0-9]{2}\/[0-9]{2}\/[1-2][0-9]{3}/)
+            })
+        })
+
+    })
+
 })
